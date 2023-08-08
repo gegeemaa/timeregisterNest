ef79e42e5f14b513935d75254b0aeb4229d2df63
gegeemaa <gerelmaaskh@gmail.com>
2023-08-08 16:53:13 +0200

    Add CORS

--- src/timeregister/timeregister.controller.ts
+++ src/timeregister/timeregister.controller.ts
@@ -3,27 +3,30 @@ import { Timeregister } from "./timeregiter.model";
 import { TimeregisterService } from "./timeregister.service";
 
 @Controller('api/v1/timeregister')
-export class TimeregisterController{
-    constructor(private readonly timeregisterService: TimeregisterService){}
+export class TimeregisterController {
+    constructor(private readonly timeregisterService: TimeregisterService) { }
 
     @Get()
-    async getAllTimeregister():Promise<Timeregister[]>{
+    async getAllTimeregister(): Promise<Timeregister[]> {
         return this.timeregisterService.getAllTimeregister()
     }
     @Post()
-    async postTimeregister(@Body() timeregisterData: Timeregister):Promise<Timeregister>{
+    async postTimeregister(@Body() timeregisterData: Timeregister): Promise<Timeregister> {
+        console.log(timeregisterData);
+        console.log("------------------------------");
+
         return this.timeregisterService.createTimeregister(timeregisterData)
     }
     @Get(':id')
-    async getTimeregister(@Param('id') id: number):Promise<Timeregister | null>{
+    async getTimeregister(@Param('id') id: number): Promise<Timeregister | null> {
         return this.timeregisterService.getTimeregister(id)
     }
     @Delete(':id')
-    async deleteTimeregister(@Param('id') id: number):Promise<Timeregister >{
+    async deleteTimeregister(@Param('id') id: number): Promise<Timeregister> {
         return this.timeregisterService.deleteTimeregister(id)
     }
     @Put(':id')
-    async updateTimeregister(@Param('id') id: number, @Body() timeregisterData: Timeregister):Promise<Timeregister >{
-        return this.timeregisterService.updateTimeregister(id,timeregisterData)
+    async updateTimeregister(@Param('id') id: number, @Body() timeregisterData: Timeregister): Promise<Timeregister> {
+        return this.timeregisterService.updateTimeregister(id, timeregisterData)
     }
 }
